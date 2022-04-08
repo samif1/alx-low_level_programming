f345ee42913f020394f0522ee935363687fc209a
sami <sam@gmail.com>
2022-04-08 15:15:16 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,10 +12,12 @@ int main(void)
 	{
 		putchar(n);
 		if (n != 57)
-		{	
+		{
+															
 			putchar(',');											
-			putchar(' ');
-		}                                                                  
+			putchar(' ');											
+		
+		}
 	}
 	putchar('\n');
 	return (0);
