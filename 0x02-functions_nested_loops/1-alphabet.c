5c50d6dd9808ff0751b593df73813578c1869702
samuel <slemma071@gmail.com>
2022-04-12 20:57:36 +0300

    alphabetical order

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+
+  
+#include "main.h"
+
+/**
+ * print_alphabet  - Entry Point
+ * Description: prints the alphabet, in lowercase, followed by a new line
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char ch;
+
+	ch = 'a';
+
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
+
+	_putchar('\n');
+}
