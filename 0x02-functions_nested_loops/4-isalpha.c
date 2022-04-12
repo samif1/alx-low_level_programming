d4ef5f784e8dad9731702a9fc27ac03ff9ec5787
samuel <slemma071@gmail.com>
2022-04-12 21:28:14 +0300

    alphabetic lower

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,18 +1,30 @@
 #include "main.h"
 
 /**
-* print_alphabet - Print the alphabet in lowercase.
-*
-* Return: void.
-*/
-
-void print_alphabet(void)
+ * _isalpha - Entry Point
+ * Description: function that checks for alphabetic character
+ * @c: input parameter
+ *
+ * Return: 1 if c is a letter, lowercase or uppercase 0 otherwise
+ */
+int _isalpha(int c)
 {
 	int i;
 
-	for (i = 97; i < 123; i++)
+	for (i = 65; i <= 90; i++)
+	{
+		if (c == i)
 		{
-		_putchar(i);
+			return (1);
 		}
-		_putchar('\n');
+	}
+
+	for (i = 97; i <= 122; i++)
+	{
+		if (c == i)
+		{
+			return (1);
+		}
+	}
+	return (0);
 }
