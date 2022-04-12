0a82b390e95bc2678a7a9f22728d8295af92fa4f
samuel <slemma071@gmail.com>
2022-04-13 02:18:19 +0300

    titable

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,51 +1,52 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
- * print_times_table - prints times table
- * @n : times table to use
- * Return:void
+ * print_times_table - prints n timestable
+ *
+ * @n: parameter
+ * Return: 0
  */
 
 void print_times_table(int n)
 {
+int i, j, mult;
 
-int a = 0, rep, b;
-
-if (n < 0 || n > 15)
-	return;
-
-while (a <= n)
+if (n >= 0 && n <= 15)
 {
-	for (b = 0; b <= n; b++)
-	{
-		rep = a * b;
-		if (b == 0)
-			_putchar('0' + rep);
-		else if (rep < 10)
-		{
-			_putchar(' ');
-			_putchar(' ');
-			_putchar('0' + rep);
-		}
-		else if (rep < 100)
-		{
-			_putchar(' ');
-			_putchar('0' + rep / 10);
-			_putchar('0' + rep % 10);
-		}
-		else
-		{
-			_putchar('0' + rep / 100);
-			_putchar('0' + (rep - 100) / 10);
-			_putchar('0' + rep % 10);
-		}
-		if (b < n)
-		{
-			_putchar(',');
-			_putchar(' ');
-		}
-	}
-	_putchar('\n');
-	a++;
+for (i = 0; i <= n; i++)
+{
+for (j = 0; j <= n; j++)
+{
+mult = i * j;
+
+if (mult < 10)
+{
+if (j >= 1)
+{
+_putchar(' ');
+_putchar((mult / 10) + ' ');
+}
+_putchar((mult % 10) + '0');
+}
+else if (mult < 100)
+{
+_putchar(' ');
+_putchar((mult / 10) + '0');
+_putchar((mult % 10) + '0');
+}
+else
+{
+_putchar((mult / 100) + '0');
+_putchar(((mult - 100) / 10) + '0');
+_putchar((mult % 10) + '0');
+}
+if (j != n)
+{
+_putchar(',');
+_putchar(' ');
+}
+}
+_putchar('\n');
+}
 }
 }
