ccea45cbfcc5b3c77617ad744583f5db312075c7
samuel <samif1@gmail.com>
2022-06-14 17:48:38 +0300

    commit

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+
+
+char *_strcpy(char *dest, char *src)
+{
+	int l1 = 0;
+	int l2 = 0;
+	int l3;
+	int i;
+
+	while (src[l1] != '\0')
+	{
+		l1++;
+	}
+	while (dest[l2] != '\0')
+	{
+		l2++;
+	}
+	l3 = l1 + l2;
+	for (i = 0; i < l3; i++)
+	{
+		dest[l1 + i] = src[i];
+		return (dest[i]);
+	}
+}
