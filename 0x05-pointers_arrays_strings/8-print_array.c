3e7eb67524e6816fb481aca32545a64a193f94ab
samuel <samif1@gmail.com>
2022-06-14 15:26:41 +0300

    commit

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,11 +11,13 @@ void print_array(int *a, int n)
 	{
 	if (i == n - 1)
 	{
-		printf("%d ", *(a + i));
+		printf("%d",*(a + i));
 	}
 	else
 	{
-		printf("%d, ", *(a + i));
+		printf("%d",*(a + i));
+		printf(",");
+		printf(" ");
 	}
 	}
 }
