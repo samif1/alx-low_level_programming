c3b856ce77f8c2ed3ff025ff5bd5b7b96767fbdc
samuel <samif1@gmail.com>
2022-06-17 13:24:08 +0300

    commit

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+
+
+
+char *_strcat(char *dest, char *src)
+{
+	int len = 0, i, j, len1 = 0;
+
+	while (src[len] != '\0')
+	{
+		len++;
+	}
+	while (dest[len1] != '\0')
+	{
+		len1++;
+	}
+	for (i = 0; i <= len1; i++)
+	{
+		dest[len + i] = src[i];
+		return (dest);
+	}
+}
