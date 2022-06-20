6e9c7fe027a7207fed15bcef106c94a33ee1223c
samuel <samif1@gmail.com>
2022-06-20 16:39:43 +0300

    commit

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -9,18 +9,7 @@
 **/
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	int len = 0;
-	int len1 = 0;
 	int i;
-
-	while (dest[len] != '\0')
-	{
-		len++;
-	}
-	while (src[len1] != '\0')
-	{
-		len1++;
-	}
 
 	for (i = 0; i < n; i++)
 	{
