5fb904986a717406e1749a09bccb8b2495738e34
samuel <samif1@gmail.com>
2022-06-20 15:58:01 +0300

    memcopy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+* _memcpy -> pointer function.
+* @dest: fird para.
+* @src: sec.
+* @n: sixe.
+* Return: dest.
+**/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int len = 0;
+	int len1 = 0;
+	int i;
+
+	while (dest[len] != '\0')
+	{
+		len++;
+	}
+	while (src[len1] != '\0'}
+	{
+		len1++;
+	}
+	for (i = 0; i < n; i++)
+	{
+		dest[len + i] = src[i];
+	}
+	return (dest);
+}
+
