f199fc6e24c100d2f65d1fa3f6d84d22a4c30271
samuel <samif1@gmail.com>
2022-07-08 16:05:12 +0300

    commit

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,13 +1,17 @@
-#ifndef variadic_functions_h 
-#define variadic_functions_h
+#ifndef VAR_FUN_H 
+#define VAR_FUN_H 
 
-#include <stdarg.h>
 #include <stdio.h>
+#include <stdarg.h>
 
 int _putchar(char c);
+
 int sum_them_all(const unsigned int n, ...);
+
 void print_numbers(const char *separator, const unsigned int n, ...);
+
 void print_strings(const char *separator, const unsigned int n, ...);
+
 void print_all(const char * const format, ...);
 
 #endif
