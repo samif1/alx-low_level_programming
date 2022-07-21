b1de8251d007c8573e71b263824749fc4ce3c7fe
samuel <samif1@gmail.com>
2022-07-21 10:29:20 +0300

    c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -33,9 +33,8 @@ list_t *add_node_end(list_t **head, const char *str)
 			{
 				temp = temp->next;
 			}
+			temp->next = ptr;
 		}
-		temp->next = ptr;
-		return (*head);
+	return (*head);
 	}
 }
-
