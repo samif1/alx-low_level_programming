6ce9307b1f1eade66f052ea622250f365fd59d6b
samuel <samif1@gmail.com>
2022-07-21 09:33:25 +0300

    commit

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+* add_node_end - the function of linked lists.
+* @head: head.
+* @str: string.
+* Return: head.
+**/
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *ptr, *temp;
+
+	ptr = malloc(sizeof(list_t));
+	if (ptr == NULL)
+		return (NULL);
+	ptr->str = strdup(str);
+	ptr->len = strlen(str);
+	ptr->next = NULL;
+	if (*head == NULL)
+	{
+		*head = ptr;
+	}
+	else
+		temp = ptr;
+	if (temp->link != NULL)
+	{
+		temp = temp->link;
+	}
+	temp->next = ptr;
+	return (*head);
+}
+
